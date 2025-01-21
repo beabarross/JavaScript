145502761c1860c8683c1c510230857db28b4467
beabarross <work.beatrizbarros@gmail.com>
2025-01-21 15:13:28 -0300

    aula 2

--- atividades/atividades.js
+++ atividades/atividades.js
@@ -8,7 +8,8 @@ function exibirMensagem() {
 
 //Exercício 3 - Calculadora
 function realizarCalculos() {
-    let numero1 = 10 , numero2 = 5;
+    let numero1 = parseInt(prompt('Informe o primeiro valor'));
+    let numero2 = parseInt(prompt('Informe o segundo valor'));
     somar(numero1,numero2);
     subtrair(numero1,numero2);
     dividir(numero1,numero2);
@@ -34,3 +35,58 @@ function multiplicar(valor1, valor2){
     let resultado = valor1 * valor2;
     console.log('O resultado da multiplicação entre '+valor1+' e '+valor2+' é : '+resultado);
 }
+
+//Calculadora "funcional"
+var valor1 = '';
+var valor2 = '';
+var operacao = '';
+const qtdCasaDecimal = 2;
+
+function guardarNumero(valor) {
+    if (!operacao) {
+        valor1 += valor;
+        console.log(valor1);
+    } else {
+        valor2 += valor;
+        console.log(valor2);
+    }
+}
+
+function guardarOperacao(operacaoE) {
+    operacao = operacaoE;
+}
+
+function limpar() {
+    valor1 = '';
+    valor2 = '';
+    operacao = '';
+}
+
+function calcular() {
+    valor1 = parseFloat(valor1);
+    valor2 = parseFloat(valor2);
+    let resultado;
+    if (operacao == '+') {
+        resultado = valor1 + valor2;
+    } else if (operacao == '-') {
+        resultado = valor1 - valor2;
+    } else if (operacao == '*') {
+        resultado = valor1 * valor2;
+    } else if (operacao == '/') {
+        resultado = valor1 / valor2;
+    }
+    alert('Resultado: ' + resultado);
+    limpar();
+}
+
+function verificarSono() {
+    const horasSono = prompt('Informe a quantidade de horas que você dorme em média:');
+    const qtdMinimaHorasSono = 8;
+    if (horasSono < qtdMinimaHorasSono) {
+        alert('Você está dormindo muito pouco.');
+    } else {
+        alert('Você está dormindo o suficiente. Parabéns!');
+    }
+}
+
+
