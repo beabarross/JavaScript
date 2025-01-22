4be94c95b577e27159428e503c3262ce7813595c
guirossbach <guilhermerossbach12@gmail.com>
2025-01-21 21:17:49 -0300

    aula 3

--- atividades/atividades.js
+++ atividades/atividades.js
@@ -79,14 +79,88 @@ function calcular() {
     limpar();
 }
 
-function verificarSono() {
-    const horasSono = prompt('Informe a quantidade de horas que você dorme em média:');
-    const qtdMinimaHorasSono = 8;
-    if (horasSono < qtdMinimaHorasSono) {
-        alert('Você está dormindo muito pouco.');
+
+//Exercício 1
+
+function identificarSono() {
+    const qtdHoras = prompt('Quantas horas você tem dormido por noite?');
+    if (qtdHoras < 8) {
+        alert('Você está dormindo pouco. Durma mais!');
     } else {
-        alert('Você está dormindo o suficiente. Parabéns!');
+        alert('Você está dormindo o suficiente, continue assim!');
     }
 }
 
+//Exercício 2
 
+/*function retornarHora(valorDaHora) {
+    const = prompt('Que horas são?')
+    if (valor da hora <= 12) {
+        alert( valorDaHora + a.m. ');
+    } else {
+        alert (' valorDaHora + p.m. ');
+    }
+}
+*/
+
+//Resolução correta Exercício 2
+
+function retornarHora() {
+    const qtdHora = prompt('Que horas são?');
+
+    if (qtdHora > 12) {
+        alert(qtdHora - 12 +'p.m.');
+    } else {
+        alert(qtdHora + 'a.m.')
+    }
+}
+
+//Explicação Operadores Lógicos
+function testarOperadorOu() {
+    const nro = prompt('Informe um número');
+    if (nro == 5 || nro == 10) {
+        alert( 'O número é 5 ou 10');
+    } else {
+        alert('Outro número');
+    }
+}
+
+function testarOperadorE() {
+    const idade = prompt('Informe a sua idade');
+    const idadeAmigo = prompt('Informe a idade do seu amigo');
+    if (idade >= 18 && idadeAmigo >= 18) {
+        alert ('Vocês estão liberados para beber');
+    } else {
+        alert ('Infelizmente não podem beber');
+    }
+}
+
+function validarNumeroEntre (vlInicio, vlFinal, vlValidar) {
+    if (vlValidar >= vlInicio && vlValidar <= vlFinal) {
+        return true;
+    }
+    return false
+}
+
+function verificarEntradaBalada() {
+    const idade = prompt('Informe a sua idade');
+    const lista = prompt('Você tem nome na lista? Digite Sim ou Não');
+    const ingresso = prompt('Você tem ingresso? Digite Sim ou Não');
+
+    if (idade >= 18 && (ingresso.toUpperCase() == 'SIM' || lista.toUpperCase() == 'SIM')) {
+        alert('Entrada permitida');
+    } else {
+        alert('Entrada negada');
+    }
+}
+
+//não é necessário criar consts, pois só são utilizado os prompts para ter informação
+
+function verificarEntradaMelhorada() {
+
+    if (prompt('Informe a sua idade') >= 18 && (prompt('Você tem nome na lista? Digite Sim ou Não').toUpperCase() == 'SIM' || prompt('Você tem ingresso? Digite Sim ou Não').toUpperCase() == 'SIM')) {
+        alert('Entrada permitida');
+    } else {
+        alert('Entrada negada');
+    }
+}
